317234c06da1a56af213f4e2f18f49b9e2075b81
tjnvr <timotheejanvier01@gmail.com>
2026-08-26 15:42:33 +0200

    Savings from previous scratches

---
+++ src/main/java/variables/Example.java
@@ -0,0 +1,23 @@
+package variables;
+
+public class Example {
+    int a;
+    String b;
+    long c = 'a';
+    int d = 0x3;
+
+    void printA() {
+        System.out.println(a);
+    }
+
+    void printB() {
+        System.out.println(b);
+    }
+
+    void printC() {
+        System.out.println(c);
+    }
+    void printD() {
+        System.out.println(d);
+    }
+}
---
+++ src/main/java/variables/Main.java
@@ -0,0 +1,13 @@
+package variables;
+
+import lombok.val;
+
+public class Main {
+    public static void main(String[] args) {
+        val ex = new Example();
+        ex.printA();
+        ex.printB();
+        ex.printC();
+        ex.printD();
+    }
+}
